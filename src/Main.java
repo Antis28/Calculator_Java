9cb59306d9de6576a7c64aeef5e1c6c3cc90b85c
Antis <antis28@mail.ru>
2023-05-11 08:25:03 +0300

    add описание требований

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,38 @@
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Требования:
+//        Калькулятор умеет выполнять операции сложения, вычитания, умножения и деления с двумя числами:
+//        a + b, a - b, a * b, a / b. Данные передаются в одну строку (смотри пример)!
+//        Решения, в которых каждое число и арифмитеческая операция передаются с новой строки считаются неверными.
+//
+//        Калькулятор умеет работать как с арабскими (1,2,3,4,5…), так и с римскими (I,II,III,IV,V…) числами.
+//
+//        Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более.
+//        На выходе числа не ограничиваются по величине и могут быть любыми.
+//
+//        Калькулятор умеет работать только с целыми числами.
+//        Калькулятор умеет работать только с арабскими или римскими цифрами одновременно,
+//        при вводе пользователем строки вроде 3 + II калькулятор должен выбросить исключение и прекратить свою работу.
+//
+//        При вводе римских чисел, ответ должен быть выведен римскими цифрами, соответственно,
+//        при вводе арабских - ответ ожидается арабскими.
+//
+//        При вводе пользователем неподходящих чисел приложение выбрасывает исключение и завершает свою работу.
+//
+//        При вводе пользователем строки, не соответствующей одной из вышеописанных арифметических операций,
+//        приложение выбрасывает исключение и завершает свою работу.
+//
+//        Результатом операции деления является целое число, остаток отбрасывается.
+//
+//        Результатом работы калькулятора с арабскими числами могут быть отрицательные числа и ноль.
+//
+//        Результатом работы калькулятора с римскими числами могут быть только положительные числа,
+//        если результат работы меньше единицы, выбрасывается исключение
 public class Main {
     public static void main(String[] args) throws IOException {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -14,12 +44,17 @@ public class Main {
         // Получаем числа
         Scanner in = new Scanner(System.in);
         System.out.println("Введите пример для решения римскими или арабскими числами: ");
-        String num1 = in.next();
+        String inputNum1 = in.next();
         String mod = in.next();
-        String num2 = in.next();
+        String inputNum2 = in.next();
 
-        CheckRomanFormat(num1, num2);
+        // Определяем формат арабский или римский
+        isRoman = isRomanFormat(inputNum1, inputNum2);
 
+        if (isRoman) {
+            RomanNumeral n1 = ConvertToRomanNumbers(num1);
+            RomanNumeral n2 = ConvertToRomanNumbers(num2);
+        }
 
         PrintInFormat((n1.getValue() / n2.getValue()));
         System.out.println(mod);
@@ -28,71 +63,117 @@ public class Main {
 
     }
 
-    private static boolean CheckRomanFormat(String num1, String num2) throws IOException {
-        boolean isRoman = false;
-        // Определяем формат арабский или римский
-        if (!isNumeric(num1)) {
-            // Вторая цифра тоже римская?
-            if (!isNumeric(num2)) {
-               isRoman = true;
-            } else {
-                throw new IOException("Цифры в разных форматах!");
-            }
-        }
-        return  isRoman;
-    }
-
     public static boolean isNumeric(String strNum) {
         if (strNum == null) {
-            return false;
+            throw new IllegalArgumentException();
         }
         try {
-            double d = Integer.parseInt(strNum);
+            Integer.parseInt(strNum);
         } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
     }
 
-    public static void PrintInFormat(RomanNumbers num, boolean isRomanFormat) {
+    public static void PrintInFormat(RomanNumeral num, boolean isRomanFormat) {
         if (isRomanFormat) {
             System.out.println(num.name());
         }
         System.out.println(num.getValue());
     }
 
-    public static String calc(String input) {
-        return input;
+    public static boolean isRomanFormat(String inputNum1, String inputNum2) throws IOException {
+        // Определяем формат арабский или римский
+        if (!isNumeric(inputNum1)) {
+            // Вторая цифра тоже римская?
+            if (!isNumeric(inputNum2)) {
+                return true;
+            } else {
+                throw new IOException(inputNum1 + " и " + inputNum2 + " - цифры в разных форматах!");
+            }
+        }
+        return false;
     }
 
-    public static RomanNumbers ConvertToRomanNumbers(String s) {
-        return switch (s) {
-            case "I", "1" -> RomanNumbers.I;
-            case "II", "2" -> RomanNumbers.II;
-            case "III", "3" -> RomanNumbers.III;
-            case "IV", "4" -> RomanNumbers.IV;
-            case "V", "5" -> RomanNumbers.V;
-            case "VI", "6" -> RomanNumbers.VI;
-            case "VII", "7" -> RomanNumbers.VII;
-            case "VIII", "8" -> RomanNumbers.VIII;
-            case "IX", "9" -> RomanNumbers.IX;
-            case "X", "10" -> RomanNumbers.X;
-            default -> throw new IllegalArgumentException("Строка не цифра из допустимого диапазона");
-        };
+    public static String calc(String mod, int num1,int num2) {
+        switch (mod) {
+            case "+": return String.valueOf (num1 + num2);
+            case "-": return String.valueOf (num1- num2);
+            case "*": return String.valueOf (num1 * num2);
+            case "/": return String.valueOf (num1 / num2);
+            default: throw
+        }
     }
 
-    enum RomanNumbers {
-        I(1), II(2), III(3), IV(4), V(5), VI(6),
-        VII(7), VIII(8), IX(9), X(10);
+    // из римских чисел в арабские
+    public static int romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
+        int result = 0;
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            RomanNumeral symbol = romanNumerals.get(i);
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        if (romanNumeral.length() > 0) {
+            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+        }
+
+        return result;
+    }
+
+    // из арабских чисел в римские
+    public static String arabicToRoman(int number) {
+        if ((number <= 0) || (number > 4000)) {
+            throw new IllegalArgumentException(number + " is not in range (0,4000]");
+        }
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    enum RomanNumeral {
+        I(1), IV(4), V(5), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100),
+        CD(400), D(500), CM(900), M(1000);
 
         private int value;
 
-        private RomanNumbers(int value) {
+        RomanNumeral(int value) {
             this.value = value;
         }
 
         public int getValue() {
             return value;
+        }
+
+        public static List<RomanNumeral> getReverseSortedValues() {
+            return Arrays.stream(values())
+                    .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+                    .collect(Collectors.toList());
         }
     }
 }
